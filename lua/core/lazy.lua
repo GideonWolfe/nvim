d35b9985b8dc64625c585607377ef49ca0769323
gideonwolfe <wolfeg2@wwu.edu>
2023-07-15 00:19:19 -0700

    replace kommentary

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -157,11 +157,14 @@ lazy.setup({
         -------------
 
         -- Auto commenting
+        -- {
+        --     "b3nj5m1n/kommentary",
+        --     config = function()
+        --         require("kommentary")
+        --     end,
+        -- },
         {
-            "b3nj5m1n/kommentary",
-            config = function()
-                require("kommentary")
-            end,
+            "numToStr/Comment.nvim",
         },
 
         -- auto pairs
@@ -206,6 +209,26 @@ lazy.setup({
             "AckslD/nvim-neoclip.lua",
             config = function()
                 require('neoclip').setup()
+            end,
+        },
+
+        -- LaTex syntax/compilation
+        {
+            'lervag/vimtex',
+            config = function()
+                local g = vim.g
+                g.vimtex_view_method = 'zathura'
+                g.latex_view_general_viewer = 'zathura'
+                g.tex_flavor = 'latex'
+                g.vimtex_complete_enabled = 1
+                -- g.vimtex_quickfix_method = 'latexmk'
+                g.vimtex_compiler_progname = 'nvr'
+                -- g.tex_conceal = ''
+                -- g.vimtex_quickfix_mode = 2
+                -- g.vimtex_view_forward_search_on_start = 0
+                -- g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
+                -- -- This must be a dictionary, and {} gets converted to a list
+                -- g.vimtex_syntax_conceal = { foo = false }
             end,
         },
 
@@ -429,27 +452,13 @@ lazy.setup({
             end,
         },
 
-        -- LaTex syntax/compilation
+        -- Keybind preview and registration
         {
-            'lervag/vimtex',
+            "folke/which-key.nvim",
             config = function()
-                local g = vim.g
-                g.vimtex_view_method = 'zathura'
-                g.latex_view_general_viewer = 'zathura'
-                g.tex_flavor = 'latex'
-                g.vimtex_complete_enabled = 1
-                -- g.vimtex_quickfix_method = 'latexmk'
-                g.vimtex_compiler_progname = 'nvr'
-                -- g.tex_conceal = ''
-                -- g.vimtex_quickfix_mode = 2
-                -- g.vimtex_view_forward_search_on_start = 0
-                -- g.vimtex_view_general_options = [[--unique file:@pdf\#src:@line@tex]]
-                -- -- This must be a dictionary, and {} gets converted to a list
-                -- g.vimtex_syntax_conceal = { foo = false }
+                require("whichkey")
             end,
         },
-
-
 
         ---------------
         -- Utilities --
