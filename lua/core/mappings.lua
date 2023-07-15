bb2fe1b5626aace583bfa662893a693a276fd31a
gideonwolfe <wolfeg2@wwu.edu>
2023-07-15 02:41:22 -0700

    maps

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -118,14 +118,14 @@ nmap('<c-l>', '<c-w>l')
 
 -- TODO why did these stop working?!!
 -- Dial mappings (smarrt increment/decrement)
--- nmap("<C-a>", "<Plug>(dial-increment)")
--- nmap("<C-x>", "<Plug>(dial-decrement)")
--- nmap("g<C-a>", "g<Plug>(dial-increment)")
--- nmap("g<C-x>", "g<Plug>(dial-decrement)")
--- vmap("<C-a>", "<Plug>(dial-increment)")
--- vmap("<C-x>", "<Plug>(dial-decrement)")
--- vmap("g<C-a>", "g<Plug>(dial-increment)")
--- vmap("g<C-x>", "g<Plug>(dial-decrement)")
+nmap("<C-a>", "<Plug>(dial-increment)")
+nmap("<C-x>", "<Plug>(dial-decrement)")
+nmap("g<C-a>", "g<Plug>(dial-increment)")
+nmap("g<C-x>", "g<Plug>(dial-decrement)")
+vmap("<C-a>", "<Plug>(dial-increment)")
+vmap("<C-x>", "<Plug>(dial-decrement)")
+vmap("g<C-a>", "g<Plug>(dial-increment)")
+vmap("g<C-x>", "g<Plug>(dial-decrement)")
 
 
 -- Markdown preview toggle
