a9fc4dc118acc41f5ac184b1dfd8ea85c0c1e64a
Gideon Wolfe <wolfegideon@gmail.com>
2024-01-29 20:45:32 -0500

    fixed luals config

--- lua/lsp/lspconfig.lua
+++ lua/lsp/lspconfig.lua
@@ -65,7 +65,8 @@ lspconfig.cssls.setup { on_attach = require("lsp-format").on_attach }
 lspconfig.cssmodules_ls.setup { on_attach = require("lsp-format").on_attach }
 
 -- Deno (JS)
-lspconfig.denols.setup { on_attach = require("lsp-format").on_attach }
+-- lspconfig.denols.setup { on_attach = require("lsp-format").on_attach }
+lspconfig.denols.setup { }
 
 -- Dockerfiles
 lspconfig.dockerls.setup { on_attach = require("lsp-format").on_attach }
@@ -150,22 +151,33 @@ lspconfig.yamlls.setup { on_attach = require("lsp-format").on_attach }
 
 
 -- Lua
-lspconfig.lua_ls.setup({
-    --on_attach = require("lsp-format").on_attach, -- specifying to use the formatter
-    settings = {
+lspconfig.lua_ls.setup {
+  on_init = function(client)
+    local path = client.workspace_folders[1].name
+    if not vim.loop.fs_stat(path..'/.luarc.json') and not vim.loop.fs_stat(path..'/.luarc.jsonc') then
+      client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
         Lua = {
-            diagnostics = {
-                globals = { "vim" },
-            },
-            workspace = {
-                -- Make the server aware of Neovim runtime files
-                library = vim.api.nvim_get_runtime_file("", true),
-                -- Hide annoying workspace questions when file opens
-                checkThirdParty = false
-            },
-            telemetry = {
-                enable = false,
-            },
-        },
-    },
-})
+          runtime = {
+            -- Tell the language server which version of Lua you're using
+            -- (most likely LuaJIT in the case of Neovim)
+            version = 'LuaJIT'
+          },
+          -- Make the server aware of Neovim runtime files
+          workspace = {
+            checkThirdParty = false,
+            library = {
+              vim.env.VIMRUNTIME
+              -- "${3rd}/luv/library"
+              -- "${3rd}/busted/library",
+            }
+            -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+            -- library = vim.api.nvim_get_runtime_file("", true)
+          }
+        }
+      })
+
+      client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
+    end
+    return true
+  end
+}
